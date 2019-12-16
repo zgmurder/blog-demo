70349fea0c324f7f105616b7f4eee5fd436ead8c
zgmurder <294069733@qq.com>
2019-12-16 17:32:10 +0800

    aaa

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zgmurder/blog-Demo.git master:gh-pages
+git push -f git@github.com:zgmurder/blog-demo.git master:gh-pages
 
 cd -
