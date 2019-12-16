73dd9663a9849d9537ca173f677eb3cc7a077f9b
zgmurder <294069733@qq.com>
2019-12-16 17:45:23 +0800

    aaa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
     // 你的GitHub仓库，请正确填写
     repo: 'https://github.com/zgmurder/blog-demo',
     // 自定义仓库链接文字。
-    repoLabel: 'blogDemo',
+    repoLabel: 'github',
     nav: [
         { text: 'Home', link: '/' },
         { text: 'FirstBlog', link: '/blog/FirstBlog.md' }
